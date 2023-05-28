88b6ac424161ea3d842ad95dbf2c41e6ffcaee85
mononoken <ken.maeshima@pm.me>
2023-05-27 23:35:41 -0700

    Build and migrate Creator association to Events

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_28_062548) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_28_063056) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.datetime "date"
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id", null: false
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_28_062548) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users", column: "creator_id"
 end
