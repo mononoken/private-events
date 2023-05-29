bcf85398b0eb4af51b36794df3e4e8e3ea747781
mononoken <ken.maeshima@pm.me>
2023-05-28 23:51:40 -0700

    Setup user show to display all events a user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'events/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'events#index'
+
+  resources :users, only: [:show]
+  resources :events, only: [:index]
 end
