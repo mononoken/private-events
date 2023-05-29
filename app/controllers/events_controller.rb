47a35cc694e086b3f7d8c9e825c3012156086eed
mononoken <ken.maeshima@pm.me>
2023-05-29 00:13:27 -0700

    Setup create in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: %i[]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.build_event(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location)
   end
 end
