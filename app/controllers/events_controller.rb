588de038bb112fe3ed5f454004ec06a21722df4a
mononoken <ken.maeshima@pm.me>
2023-05-29 15:40:25 -0700

    Allow index to be viewed without signing in

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except: %i[]
+  before_action :authenticate_user!, except: %i[index]
 
   def index
     @events = Event.all
